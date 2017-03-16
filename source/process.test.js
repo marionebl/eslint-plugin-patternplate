14ea2e6cdf5653947157ec88d62b6b53929ead9e
Mario Nebl <hello@herebecode.com>
2017-03-16 17:23:49 +0100

    fix: account for offsets when replacing import sources

--- source/process.test.js
+++ source/process.test.js
@@ -95,6 +95,61 @@ test('should resolve flowtype pattern references', async t => {
 	t.is(actual, expected);
 });
 
+test('should resolve named flowtype pattern references', async t => {
+	const code = stripIndent`
+		import type {someModule} from 'b';
+	`;
+
+	const expected = stripIndent`
+		import type {someModule} from '../b';
+	`;
+
+	const [actual] = process(code, neutral`fixtures/pattern/patterns/a/index.js`);
+	t.is(actual, expected);
+});
+
+test('should account for offsets when resolving multiple entries', async t => {
+	const code = stripIndent`
+		// @flow
+		import spaces from 'Pattern';
+		import type {Spaces} from 'Pattern';
+
+		export default SpacesDemo;
+	`;
+
+	const expected = stripIndent`
+		// @flow
+		import spaces from './';
+		import type {Spaces} from './';
+
+		export default SpacesDemo;
+	`;
+
+	const [actual] = process(code, neutral`fixtures/pattern/patterns/a/index.js`);
+	t.is(actual, expected);
+});
+
+test('should account for offsets when resolving multiple entries with extensions', async t => {
+	const code = stripIndent`
+		// @flow
+		import type {Spaces} from 'b';
+		import spaces from 'Pattern';
+
+		export default SpacesDemo;
+	`;
+
+	const expected = stripIndent`
+		// @flow
+		import type {Spaces} from '../b/index.jsx';
+		import spaces from './index.jsx';
+
+		export default SpacesDemo;
+	`;
+
+	const [actual] = process(code, neutral`fixtures/pattern/patterns/a/index.jsx`);
+	t.is(actual, expected);
+});
+
 test('should respect source extenseion for pattern reference', async t => {
 	const code = stripIndent`
 		import b from "b";
